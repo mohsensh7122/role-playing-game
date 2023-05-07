0d946657cbd845107ce601cf0c68c588e2b95c35
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-05-07 01:18:37 -0400

    Added the renderCharacter function.
    This function which inserts the HTML dynamically using JS.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+const heroID = "hero";
+const heroName = "Wizard";
+const heroAvatar = "images/wizard.png";
+const heroHealth = 60;
+const heroScore = 6;
+
+const monsterID = "monster";
+const monsterName = "Orc";
+const monsterAvatar = "images/orc.png";
+const monsterHealth = 10;
+const monsterScore = 4;
+
+
+function renderCharacter(elementId, name, avatar, health, score) {
+   document.getElementById(elementId).innerHTML = `<div class="character-card">
+   <h4 class="name"> ${name} </h4>
+   <img class="avatar" src="${avatar}"/>
+   <p class="health">health: <b> ${health} </b></p>
+   <div class="dice-container"><div class="dice"> ${score} </div></div>
+   </div>`;
+}
+
+renderCharacter(heroID, heroName, heroAvatar, heroHealth, heroScore);
+renderCharacter(monsterID, monsterName, monsterAvatar, monsterHealth, monsterScore);
