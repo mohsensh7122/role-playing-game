00ec8ae6d9a4c92eb8d5d69ddf0dbafd739d8258
Mohsen Shirazi Nalband <mohsensh4792@gmail.com>
2023-05-07 18:47:05 -0400

    Added the getDiceHtml function
    this functions takes in the getDiceRollArray(diceCount) function and returns the html for dicerolls with random numbers as their values.
    Also deleted the placeholder diceRoll arrays.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+function getDiceRollArray(diceCount){
+   const newDiceRolls = []
+   for(let i = 0; i < diceCount; i++){
+       newDiceRolls.push(Math.floor(Math.random()*6) +1)
+   }
+   return newDiceRolls
+}
+
 const hero = {
    elementId: "hero",
    name: "Wizard",
    avatar: "images/wizard.png",
    health: 60,
-   diceRoll: [3, 1, 4],
    diceCount: 3
 }
 
@@ -12,7 +19,6 @@ const monster = {
    name: "Orc",
    avatar: "images/orc.png",
    health: 10,
-   diceRoll: [6],
    diceCount: 1
 }
 
@@ -28,18 +34,14 @@ function renderCharacter(data) {
       diceCount
    } = data;
 
-
-   function getDiceRollArray(diceCount){
-      const newDiceRolls = []
-      for(let i = 0; i < diceCount; i++){
-          newDiceRolls.push(Math.floor(Math.random()*6) +1)
-      }
-      return newDiceRolls
+   function getDiceHtml(diceCount){
+      return getDiceRollArray(diceCount).map(diceroll => {
+          return `<div class="dice">${diceroll}</div>`
+      }).join('')
   }
-  
-   const diceHtml = diceRoll.map(function(num){
-      return `<div class="dice">${num}</div>`
-  }).join('')
+   
+
+   const diceHtml = getDiceHtml(diceCount);
 
    document.getElementById(elementId).innerHTML = `<div class="character-card">
    <h4 class="name"> ${name} </h4>
