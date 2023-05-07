8be13d8c6c8036b3eb69f1cb0fba754a6cf60cb8
Mohsen Shirazi Nalband <mohsensh4792@gmail.com>
2023-05-07 16:11:38 -0400

    Swapped out the for loop with .map()

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-
 const hero = {
    elementId: "hero",
    name: "Wizard",
    avatar: "images/wizard.png",
    health: 60,
-   score: 6
+   diceRoll: [3, 1, 4],
+   diceCount: 3
 }
 
 const monster = {
@@ -13,18 +12,33 @@ const monster = {
    name: "Orc",
    avatar: "images/orc.png",
    health: 10,
-   score: 4
+   diceRoll: [6],
+   diceCount: 1
 }
 
 
 
 function renderCharacter(data) {
-   const {elementId, name, avatar, health, score} = data;
+   const {
+      elementId,
+      name,
+      avatar,
+      health,
+      diceRoll,
+      diceCount
+   } = data;
+
+   const diceHtml = diceRoll.map(function(num){
+      return `<div class="dice">${num}</div>`
+  }).join('')
+
    document.getElementById(elementId).innerHTML = `<div class="character-card">
    <h4 class="name"> ${name} </h4>
    <img class="avatar" src="${avatar}"/>
    <p class="health">health: <b> ${health} </b></p>
-   <div class="dice-container"><div class="dice"> ${score} </div></div>
+   <div class="dice-container">
+      ${diceHtml}
+   </div>
    </div>`;
 }
 
