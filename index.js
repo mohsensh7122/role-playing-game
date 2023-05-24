1372090bf9572877998770540427ebb14566e8db
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-05-24 15:48:24 -0400

    added the getCharacterHtml method

--- index.js
+++ index.js
@@ -27,6 +27,20 @@ function Character(){
    this.avatar = data.avatar;
    this.health = data.health;
    this.diceCount = data.diceCount;
+
+   this.getCharacterHtml = function () {
+      const { elementId, name, avatar, health, diceCount } = this;
+      const diceHtml = getDiceHtml(diceCount);
+
+      document.getElementById(elementId).innerHTML =
+          `<div class="character-card">
+          <h4 class="name"> ${name} </h4>
+          <img class="avatar" src="${avatar}" />
+          <div class="health">health: <b> ${health} </b></div>
+          <div class="dice-container">    
+              ${diceHtml}
+          </div>
+      </div>`;
 }
 
 function renderCharacter(data) {
@@ -58,5 +72,4 @@ function renderCharacter(data) {
    </div>`;
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
+
